20841dbfc50d72665c9be6e298b49464eb5727a7
sato <sato@sato-virtual-machine.(none)>
2013-03-14 13:08:25 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ham::Application.config.secret_token = '58623b422e3e67e8f7266a03e40be24ab3ac55d8c1d246c3f117a386dfba6b99c35fb50157f4e12c91bcd7d7509a978fa26d231b8b147cece0d4d368bd09a6d8'
